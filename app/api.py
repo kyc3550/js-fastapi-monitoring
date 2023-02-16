dfb9a1f5d5673880bfc3dad6b7065330dfe8c42c
kyc3550 <rmawkxkq0909@naver.com>
2023-02-16 13:55:18 +0900

    first commit

--- app/api.py
+++ app/api.py
@@ -15,3 +15,17 @@ model = load(ROOT_DIR / "artifacts/model.joblib")
 @app.get("/")
 def root():
     return "Wine Quality Ratings"
+
+@app.post("/predict", response_model=Rating)
+def predict(response: Response, sample: Wine):
+    sample_dict = sample.dict()
+    features = np.array([sample_dict[f] for f in feature_names]).reshape(1, -1)
+    features_scaled = scaler.transform(features)
+    prediction = model.predict(features_scaled)[0]
+    response.headers["X-model-score"] = str(prediction)
+    return Rating(quality=prediction)
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return {"status": "ok"}
